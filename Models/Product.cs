68bb3add5cc5437d346027f91c3d54b534c419f2
quangminh4102008 <144684151+quangminh4102008@users.noreply.github.com>
2024-12-25 20:05:22 +0700

    Delete Models directory

--- Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication5.Models;
-
-public class Product
-{
-    public long Id { get; set; }
-    public String Name { get; set; }
-    public long Price { get; set; }
-    public virtual List<OrderDetail> OrderDetails { get; set; }
-}
--- Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication5.Models;
-
-public class User
-{
-    public long Id { get; set; }
-    public String Email { get; set; }
-    public String Name { get; set; }
-    public virtual List<Order> Orders { get; set; }
-}
